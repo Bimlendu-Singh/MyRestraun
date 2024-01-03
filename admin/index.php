7592e369d084691abdc622ab559d9b39bdb44055
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-01-03 15:36:12 +0530

    admin updated

--- admin/index.php
+++ admin/index.php
@@ -1,8 +1,16 @@
 <html>
 <head>
     <title>MyRestraun -Home Page</title>
+    <link rel="stylesheet" href="../css/admin.css">
+
+        <!-- Link of font family included -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Caveat&family=Foldit:wght@300&family=Roboto&display=swap" rel="stylesheet">
+
 </head>
+
 <body>
-    
+    <h1>Hello</h1>
 </body>
 </html>

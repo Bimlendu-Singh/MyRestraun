00e2f0d97e76c7790cf01e231f7ebd51d910fa6c
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-01-03 19:36:53 +0530

    Dashboard added

--- admin/index.php
+++ admin/index.php
@@ -30,7 +30,39 @@
 <!-- Menu content Section Starts Here  -->
 <div class="main-content">
     <div class="wrapper">
+        <strong>DASHBOARD</strong>
 
+        <div class="col-4 text-allign-center">
+
+            <h1>5</h1>
+            <br>
+            Categories
+
+        </div>
+
+        <div class="col-4 text-allign-center">
+
+            <h1>5</h1>
+            <br>
+            Categories
+
+        </div>
+
+        <div class="col-4 text-allign-center">
+
+            <h1>5</h1>
+            <br>
+            Categories
+
+        </div>
+
+        <div class="col-4 text-allign-center">
+
+            <h1>5</h1>
+            <br>
+            Categories
+
+        </div>
     </div>
 </div>
 <!-- Menu content Section Ends Here  -->

f70dac7e282409277a72a0f6c30901415f001c19
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-01-04 22:20:31 +0530

    manage category,order,food updated

--- admin/manage-food.php
+++ admin/manage-food.php
@@ -5,6 +5,51 @@
     <div class="wrapper overflow">
         <h2>Manage Food</h2>
      
+        <br>
+
+        <!-- Button to Add Admin -->
+        <a class="button" href="">Add Food</a>
+        <br>
+        <br>
+     
+    <table class="tbl-width-full">
+        <tr>
+            <th>S.N.</th>
+            <th>Full Name</th>
+            <th>Username</th>
+            <th>Ations</th>
+        </tr>
+
+        <tr>
+            <td>1.</td>
+            <td>Bheem Singh</td>
+            <td>bheemsingh</td>
+            <td>
+            <a class="btn-primary" href="">Update Admin</a>
+            <a class="btn-secondary" href="">Delete Admin</a>
+            </td>
+        </tr>
+
+        <tr>
+            <td>1.</td>
+            <td>Bheem Singh</td>
+            <td>bheemsingh</td>
+            <td>
+            <a class="btn-primary" href="">Update Admin</a>
+            <a class="btn-secondary" href="">Delete Admin</a>
+            </td>
+        </tr>
+        
+        <tr>
+            <td>1.</td>
+            <td>Bheem Singh</td>
+            <td>bheemsingh</td>
+            <td>
+            <a class="btn-primary" href="">Update Admin</a>
+            <a class="btn-secondary" href="">Delete Admin</a>
+            </td>
+        </tr>
+    </table>
     </div>
 </div>
 <!-- Main content Section Ends Here  -->
--- admin/manage-order.php
+++ admin/manage-order.php
@@ -5,6 +5,46 @@
     <div class="wrapper overflow">
         <h2>Manage Order</h2>
      
+        <br>
+
+    <table class="tbl-width-full">
+        <tr>
+            <th>S.N.</th>
+            <th>Full Name</th>
+            <th>Username</th>
+            <th>Ations</th>
+        </tr>
+
+        <tr>
+            <td>1.</td>
+            <td>Bheem Singh</td>
+            <td>bheemsingh</td>
+            <td>
+            <a class="btn-primary" href="">Update Admin</a>
+            <a class="btn-secondary" href="">Delete Admin</a>
+            </td>
+        </tr>
+
+        <tr>
+            <td>1.</td>
+            <td>Bheem Singh</td>
+            <td>bheemsingh</td>
+            <td>
+            <a class="btn-primary" href="">Update Admin</a>
+            <a class="btn-secondary" href="">Delete Admin</a>
+            </td>
+        </tr>
+        
+        <tr>
+            <td>1.</td>
+            <td>Bheem Singh</td>
+            <td>bheemsingh</td>
+            <td>
+            <a class="btn-primary" href="">Update Admin</a>
+            <a class="btn-secondary" href="">Delete Admin</a>
+            </td>
+        </tr>
+    </table>
     </div>
 </div>
 <!-- Main content Section Ends Here  -->

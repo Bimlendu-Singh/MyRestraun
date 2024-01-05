6d2c5fb8ce8b749786bdacf814d657f8babfcf46
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-01-05 19:07:59 +0530

    Delete Admin page started

---
+++ admin/delete-admin.php
@@ -0,0 +1,10 @@
+<?php
+
+    // 1. Get the ID of the Admin to be Deleted
+
+    // 2. Create SQL Query to Delete Admin
+
+    // 3. Redirect to Manage Admin page with message (sucess/error)
+
+
+?>

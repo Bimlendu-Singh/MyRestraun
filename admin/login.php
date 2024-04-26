95cc37f990d37a4a40d741e5d3f423951ac20ff4
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-04-26 18:01:36 +0530

    Makin login authentication page for myRestraun 1st commit of this page

---
+++ admin/login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login- MyRestraun System</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+    
+</body>
+</html>

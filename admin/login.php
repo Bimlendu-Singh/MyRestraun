1964cc43e5e186d82120aeea567edb58a4436ba3
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-04-27 12:29:45 +0530

    login page uddate

--- admin/login.php
+++ admin/login.php
@@ -5,8 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login- MyRestraun System</title>
     <link rel="stylesheet" href="../css/admin.css">
+
 </head>
+
 <body>
     
+        <div class="login">
+            <h1>Login</h1>
+
+            <p>Created By - <a href="#"></a>Bimlendu Singh</p>
+        </div>
+
 </body>
 </html>

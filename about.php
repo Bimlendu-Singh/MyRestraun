0417c6862632bdd6f1f5678b93c6e285e02016b9
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-08-14 12:10:16 +0530

    contact page updated

--- about.php
+++ about.php
@@ -1,50 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Bheem's Restaurant</title>
-    <link rel="stylesheet" href="css/style.css" />
+<?php include('partials-front/menu.php') ?>
 
-    <!-- Site Icons -->
-    <link rel="shortcut icon" href="image/favicon.ico" type="image/x-icon" />
-    <link rel="apple-touch-icon" href="image/apple-touch-icon.png" />
-
-    <!-- Link of font family included -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Caveat&family=Foldit:wght@300&family=Roboto&display=swap"
-      rel="stylesheet"
-    />
-  </head>
-
-  <body>
-    <!-- Navbar Section Starts Here -->
-    <section class="Navbar">
-      <div class="container navbar-container">
-        <div class="logo">
-          <img
-            class="responsive-img"
-            src="image/logo/logo 15.png"
-            alt="Restaurant Logo"
-          />
-        </div>
-
-        <div class="menu text-allign-center">
-          <ul>
-            <li><a href="index.html">Home</a></li>
-            <li><a href="foods.html">Foods</a></li>
-            <li><a href="categories.html">Categories</a></li>
-            <li><a href="about.html">About</a></li>
-            <li><a href="contact.html">Contact</a></li>
-          </ul>
-        </div>
-
-        <div class="clearfix"></div>
-      </div>
-    </section>
-    <!-- Navbar Section Ends Here -->
 
     <!-- About Section Starts Here -->
 
@@ -77,39 +32,4 @@
 
     <!-- About Section Starts Here -->
 
-    <!-- Social Media Section Starts Here -->
-    <section class="social text-allign-center">
-      <div class="container">
-        <ul>
-          <li>
-            <a href="http://www.facebook.com"
-              ><img src="./image/fbicon.png" alt="Facebook"
-            /></a>
-          </li>
-          <li>
-            <a href="http://www.instagram.com"
-              ><img src="./image/instaicon.png" alt="Instagram"
-            /></a>
-          </li>
-          <li>
-            <a href="http://www.twitter.com"
-              ><img src="./image/twittericon.gif" alt="Twitter"
-            /></a>
-          </li>
-        </ul>
-      </div>
-    </section>
-    <!-- Social Media Section Ends Here -->
-
-    <!-- Footer Section Starts Here -->
-    <section class="Footer">
-      <div class="container text-allign-center">
-        <p>
-          All rights reserved. Designed By
-          <a href="">Bheem Singh And Amreen Wafa</a>
-        </p>
-      </div>
-    </section>
-    <!-- Footer Section Ends Here -->
-  </body>
-</html>
+    <?php include('partials-front/footer.php') ?>
--- contact.php
+++ contact.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
@@ -8,9 +8,9 @@
 
     <title>Contact Us</title>
   </head>
-  <body>
+  <body> -->
     <!-- Navbar Section Starts Here -->
-    <section class="Navbar">
+    <!-- <section class="Navbar">
       <div class="container navbar-container">
         <div class="logo">
           <img
@@ -22,18 +22,69 @@
 
         <div class="menu text-allign-center">
           <ul>
-            <li><a href="index.html">Home</a></li>
-            <li><a href="foods.html">Foods</a></li>
-            <li><a href="categories.html">Categories</a></li>
-            <li><a href="about.html">About</a></li>
-            <li><a href="contact.html">Contact</a></li>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="foods.php">Foods</a></li>
+            <li><a href="categories.php">Categories</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
           </ul>
         </div>
 
         <div class="clearfix"></div>
       </div>
-    </section>
+    </section> -->
     <!-- Navbar Section Ends Here -->
+
+
+
+
+    <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Bheem's Restaurant</title>
+    <link rel="stylesheet" href="css/contact.css" />
+    <link rel="stylesheet" href="css/style.css">
+
+
+    <!-- Site Icons -->
+    <link rel="shortcut icon" href="image/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="image/apple-touch-icon.png">
+
+    <!-- Link of font family included -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Caveat&family=Foldit:wght@300&family=Roboto&display=swap" rel="stylesheet">
+  </head>
+
+
+  <body>
+
+    <!-- Navbar Section Starts Here -->
+     <section class="Navbar">
+        <div class="container navbar-container">
+            <div class="logo">
+                <img class="responsive-img" src="image/logo/logo 15.png" alt="Restaurant Logo">
+            </div>
+            
+            <div class="menu text-allign-center" >
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="foods.php">Foods</a></li>
+                    <li><a href="categories.php">Categories</a></li>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="contact.php">Contact</a></li>                    
+                </ul>
+            </div>
+
+            <div class="clearfix"></div>
+        </div>
+     </section>
+    <!-- Navbar Section Ends Here -->
+    <br>
+
+
 
     <div class="container">
       <h1 class="text-allign-center" style="color: rgb(255, 106, 0)">
@@ -95,13 +146,30 @@
       <!-- End .wrapper -->
     </div>
     <!-- End .container -->
+    <br>
+
+        <!-- Social Media Section Starts Here -->
+        <section class="social text-allign-center">
+        <div class="">
+            <ul>
+                <li><a href="http://www.facebook.com"><img src="./image/fbicon.png" alt="Facebook"></a></li>
+                <li><a href="http://www.instagram.com"><img src="./image/instaicon.png" alt="Instagram"></a></li>
+                <li><a href="http://www.twitter.com"><img src="./image/twittericon.gif" alt="Twitter"></a></li>
+
+            </ul>
+        </div>
+     </section>
+    <!-- Social Media Section Ends Here -->
+    <br>
 
     <!-- Footer Section Starts Here -->
     <section class="Footer">
-      <div class="container text-allign-center">
-        <p>All rights reserved. Designed By <a href="">Bheem Singh</a></p>
+      <div class=" text-allign-center">
+        <p>All rights reserved. Designed By <a href="">Bheem Singh And Amreen Wafa</a></p>
       </div>
     </section>
+    <br>
+    
     <!-- Footer Section Ends Here -->
   </body>
 </html>
